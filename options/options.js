6c09f5a403c1d96adc12fc4d2dab44552f7f79ce
Crowdbotics <team@crowdbotics.com>
2022-06-09 02:09:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "social_money_35647",
+  url: "https://social_money_35647.botics.co",
+  api: "https://social_money_35647.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
